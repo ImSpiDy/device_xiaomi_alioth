c432aac57c85725d491847da0c2d7857295442f5
ImSpiDy <SpiDy2713@gmail.com>
2024-07-17 04:08:56 +0530

    Revert "alioth: Aospify"
    This reverts commit a1b6f5a8287e39c105c688389ede163b628ee850.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -9,23 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelOs stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
-
-# Environment Flags
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
